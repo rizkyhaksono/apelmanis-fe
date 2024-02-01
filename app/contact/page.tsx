222c5f521840d75770ef096ee6f80f0a4bf0d12c
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-02-01 10:33:36 +0800

    fix (contact): change name and links

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -14,17 +14,17 @@ export default function ContactPage() {
           <div className="grid xl:grid-cols-2 lg:grid-cols-1 gap-3">
             <div className="flex items-center gap-3">
               <Image alt="nextui logo" height={40} radius="sm" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/100px-Gmail_icon_%282020%29.svg.png" width={40} />
-              <a href="mailto:novitalencanawati10@gmail.com" target="_blank">
+              <a href="mailto:mrizkyhaksono@gmail.com" target="_blank">
                 <p className="text-md text-start">Email</p>
-                <p className="text-small text-default-500">novitalencanawati10@gmail.com</p>
+                <p className="text-small text-default-500">mrizkyhaksono@gmail.com</p>
               </a>
             </div>
 
             <div className="flex items-center gap-3">
               <Image alt="nextui logo" height={40} radius="sm" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/2048px-Instagram_logo_2022.svg.png" width={40} />
-              <a href="https://instagram.com/novita_lencanawati" target="_blank">
+              <a href="https://instagram.com/rizkyhaksonoo" target="_blank">
                 <p className="text-md text-start">Instagram</p>
-                <p className="text-small text-default-500">@novita_lencanawati</p>
+                <p className="text-small text-default-500">@rizkyhaksonoo</p>
               </a>
             </div>
           </div>
